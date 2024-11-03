b841bef3f84525703ea2c3f25c3a95c0d5f3dcf5
LuK1337 <priv.luk@gmail.com>
2024-11-03 09:00:52 +0100

    Add support for GitHub license URLs

--- src/main/java/org/lineageos/generatebp/models/License.kt
+++ src/main/java/org/lineageos/generatebp/models/License.kt
@@ -1,5 +1,5 @@
 /*
- * SPDX-FileCopyrightText: 2023 The LineageOS Project
+ * SPDX-FileCopyrightText: 2023-2024 The LineageOS Project
  * SPDX-License-Identifier: Apache-2.0
  */
 
@@ -7,20 +7,24 @@ package org.lineageos.generatebp.models
 
 enum class License(
     val spdxId: String,
-    val urlRegex: Regex,
+    vararg val urlRegex: Regex,
 ) {
     APACHE_2_0(
         "Apache-2.0",
-        Regex(".*www.apache.org/licenses/LICENSE-2.0\\..*")
+        Regex(".*www.apache.org/licenses/LICENSE-2.0\\..*"),
+        Regex("http(s)?://api.github.com/licenses/apache-2.0"),
     ),
     GPL_3_0(
         "GPL-3.0",
-        Regex(".*www.gnu.org/licenses/gpl-3.0\\..*")
+        Regex(".*www.gnu.org/licenses/gpl-3.0\\..*"),
+        Regex("http(s)?://api.github.com/licenses/gpl-3.0"),
     );
 
     companion object {
         fun fromUrl(url: String) = values().firstOrNull {
-            it.urlRegex.matches(url)
+            it.urlRegex.any { regex ->
+                regex.matches(url)
+            }
         }
     }
 }
