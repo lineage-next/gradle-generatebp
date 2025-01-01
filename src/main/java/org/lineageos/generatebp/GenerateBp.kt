f4033e70c8092585078e4f6108e0a2fae30cfecd
Luca Stefani <luca.stefani.ge1@gmail.com>
2025-01-01 16:17:24 +0100

    Only add inception year to .license

--- src/main/java/org/lineageos/generatebp/GenerateBp.kt
+++ src/main/java/org/lineageos/generatebp/GenerateBp.kt
@@ -60,7 +60,6 @@ internal class GenerateBp(
                 initialYear = null,
                 addNewlineBetweenCopyrightAndLicense = false,
                 addEndingNewline = false,
-                addCurrentYear = false,
             ).prependIndent("// ")
         )
         append("\n")
