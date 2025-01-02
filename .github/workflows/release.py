cf2233c7b97a610eebbba7b1a3476d3ded959b56
LuK1337 <priv.luk@gmail.com>
2025-01-02 09:31:11 +0100

    Add workflow for making a release

---
+++ .github/workflows/release.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+import subprocess
+
+with open("build.gradle.kts", "r+") as f:
+    lines = ""
+
+    for line in f.readlines():
+        if line.startswith("version = "):
+            version = line[11:-2]
+            version_new = (
+                f'{version.split(".")[0]}.{str(int(version.split(".")[1]) + 1)}'
+            )
+            line = f'version = "{version_new}"\n'
+
+        lines += line
+
+    f.seek(0)
+    f.write(lines)
+
+with open("example/app/build.gradle.kts", "r+") as f:
+    lines = f.read().replace(f"/v{version}/", f"/v{version_new}/")
+    f.seek(0)
+    f.write(lines)
+
+subprocess.run(["git", "tag", f"v{version}"])
+subprocess.run(["git", "add", "build.gradle.kts", "example/app/build.gradle.kts"])
+subprocess.run(["git", "commit", "-m", "Bump version"])
