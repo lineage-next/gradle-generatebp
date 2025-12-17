40a0b345bc63505afff91a22af64c12074019148
LuK1337 <priv.luk@gmail.com>
2025-12-17 20:55:21 +0100

    Set IGNORE_CASE regex option for license URLs

--- src/main/java/org/lineageos/generatebp/models/License.kt
+++ src/main/java/org/lineageos/generatebp/models/License.kt
@@ -11,33 +11,69 @@ enum class License(
 ) {
     APACHE_2_0(
         "Apache-2.0",
-        Regex(".*www.apache.org/license(s)?/LICENSE-2.0\\..*"),
-        Regex("http(s)?://api.github.com/licenses/apache-2.0"),
+        Regex(
+            ".*www.apache.org/license(s)?/LICENSE-2.0\\..*",
+            setOf(RegexOption.IGNORE_CASE)
+        ),
+        Regex(
+            "http(s)?://api.github.com/licenses/apache-2.0",
+            setOf(RegexOption.IGNORE_CASE)
+        ),
     ),
     BSD_2_CLAUSE(
         "BSD-2-Clause",
-        Regex("http(s)?://(www\\.)?opensource.org/licenses/bsd-2-clause"),
-        Regex("http(s)?://api.github.com/licenses/bsd-2-clause"),
+        Regex(
+            "http(s)?://(www\\.)?opensource.org/licenses/bsd-2-clause",
+            setOf(RegexOption.IGNORE_CASE)
+        ),
+        Regex(
+            "http(s)?://api.github.com/licenses/bsd-2-clause",
+            setOf(RegexOption.IGNORE_CASE)
+        ),
     ),
     BSD_3_CLAUSE(
         "BSD-3-Clause",
-        Regex("http(s)?://(www\\.)?opensource.org/licenses/bsd-(license|3-clause)"),
-        Regex("http(s)?://api.github.com/licenses/bsd-3-clause"),
+        Regex(
+            "http(s)?://(www\\.)?opensource.org/licenses/bsd-(license|3-clause)",
+            setOf(RegexOption.IGNORE_CASE)
+        ),
+        Regex(
+            "http(s)?://api.github.com/licenses/bsd-3-clause",
+            setOf(RegexOption.IGNORE_CASE)
+        ),
     ),
     GPL_3_0(
         "GPL-3.0",
-        Regex(".*www.gnu.org/licenses/gpl-3.0\\..*"),
-        Regex("http(s)?://api.github.com/licenses/gpl-3.0"),
+        Regex(
+            ".*www.gnu.org/licenses/gpl-3.0\\..*",
+            setOf(RegexOption.IGNORE_CASE)
+        ),
+        Regex(
+            "http(s)?://api.github.com/licenses/gpl-3.0",
+            setOf(RegexOption.IGNORE_CASE)
+        ),
     ),
     LGPL_3_0(
         "LGPL-3.0",
-        Regex(".*www.gnu.org/licenses/lgpl-3.0\\..*"),
-        Regex("http(s)?://api.github.com/licenses/lgpl-3.0"),
+        Regex(
+            ".*www.gnu.org/licenses/lgpl-3.0\\..*",
+            setOf(RegexOption.IGNORE_CASE)
+        ),
+        Regex(
+            "http(s)?://api.github.com/licenses/lgpl-3.0",
+            setOf(RegexOption.IGNORE_CASE)
+        ),
     ),
     MIT(
         "MIT",
-        Regex("http(s)?://opensource.org/licenses/MIT"),
-        Regex("http(s)?://api.github.com/licenses/mit"),
+        Regex(
+            "http(s)?://opensource.org/licenses/MIT",
+            setOf(RegexOption.IGNORE_CASE)
+        ),
+        Regex(
+            "http(s)?://api.github.com/licenses/mit",
+            setOf(RegexOption.IGNORE_CASE)
+        ),
     );
 
     companion object {
